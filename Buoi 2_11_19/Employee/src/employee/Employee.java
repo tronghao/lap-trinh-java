32ed6935ec0fe625acb179a54f99cdef8df3fbac
Trọng Hảo <49613788+tronghao@users.noreply.github.com>
2019-11-02 10:29:11 +0700

    Add files via upload

---
+++ Buoi 2_11_19/Employee/src/employee/Employee.java
@@ -0,0 +1,14 @@
+
+package employee;
+
+
+public class Employee {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
